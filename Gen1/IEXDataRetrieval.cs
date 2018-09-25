923a867785e8cdcbdeee516d7520673e18786e4b
backusd <backusdakota@gmail.com>
2018-09-24 20:43:24 -0700

    Add project files.

---
+++ Gen1/IEXDataRetrieval.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gen1
+{
+    public class IEXDataProxy
+    {
+        public async static Task<List<RootObject>> GetData(string duration, string ticker)
+        {
+            var http = new HttpClient();
+            string URI = String.Format("https://api.iextrading.com/1.0/stock/{0}/chart/{1}", ticker, duration);
+            var response = await http.GetAsync(URI);
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(List<RootObject>));
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (List<RootObject>)serializer.ReadObject(ms);
+
+            return data;
+        }
+
+    }
+
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public string date { get; set; }
+
+        [DataMember]
+        public double open { get; set; }
+
+        [DataMember]
+        public double high { get; set; }
+
+        [DataMember]
+        public double low { get; set; }
+
+        [DataMember]
+        public double close { get; set; }
+
+        [DataMember]
+        public int volume { get; set; }
+
+        [DataMember]
+        public int unadjustedVolume { get; set; }
+
+        [DataMember]
+        public double change { get; set; }
+
+        [DataMember]
+        public double changePercent { get; set; }
+
+        [DataMember]
+        public double vwap { get; set; }
+
+        [DataMember]
+        public string label { get; set; }
+
+        [DataMember]
+        public double changeOverTime { get; set; }
+    }
+
+    /* This is for the original IEX data way
+    public class IEXDataRetrieval
+    {
+        public String getData()
+        {
+            var symbol = "msft";
+            var IEXTrading_API_PATH = "https://api.iextrading.com/1.0/stock/{0}/chart/1y";
+
+            IEXTrading_API_PATH = string.Format(IEXTrading_API_PATH, symbol);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                String returnString = "";
+
+                //For IP-API
+                client.BaseAddress = new Uri(IEXTrading_API_PATH);
+                HttpResponseMessage response = client.GetAsync(IEXTrading_API_PATH).GetAwaiter().GetResult();
+                if (response.IsSuccessStatusCode)
+                {
+                    var historicalDataList = response.Content.ReadAsAsync<List<HistoricalDataResponse>>().GetAwaiter().GetResult();
+                    foreach (var historicalData in historicalDataList)
+                    {
+                        if (historicalData != null)
+                        {
+                            returnString += ("Open: " + historicalData.open + "\n");
+                            returnString += ("Close: " + historicalData.close + "\n");
+                            returnString += ("Low: " + historicalData.low + "\n");
+                            returnString += ("High: " + historicalData.high + "\n");
+                            returnString += ("Change: " + historicalData.change + "\n");
+                            returnString += ("Change Percentage: " + historicalData.changePercent + "\n");
+
+                            /*
+                            Console.WriteLine("Open: " + historicalData.open);
+                            Console.WriteLine("Close: " + historicalData.close);
+                            Console.WriteLine("Low: " + historicalData.low);
+                            Console.WriteLine("High: " + historicalData.high);
+                            Console.WriteLine("Change: " + historicalData.change);
+                            Console.WriteLine("Change Percentage: " + historicalData.changePercent);
+                            
+                        }
+                    }
+
+                    return returnString;
+                }
+                else
+                    return "fail";
+            }
+        }
+
+    }
+    
+
+    public class HistoricalDataResponse
+    {
+        public string date { get; set; }
+        public double open { get; set; }
+        public double high { get; set; }
+        public double low { get; set; }
+        public double close { get; set; }
+        public int volume { get; set; }
+        public int unadjustedVolume { get; set; }
+        public double change { get; set; }
+        public double changePercent { get; set; }
+        public double vwap { get; set; }
+        public string label { get; set; }
+        public double changeOverTime { get; set; }
+    }
+    */
+
+}
---
+++ Gen1/MainPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Gen1
+{
+    public class FinancialData
+    {
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double Open { get; set; }
+        public double Close { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            GenerateChart("1m");
+        }
+
+        private async void GenerateChart(string duration)
+        {
+            this.OhlcChart.DataContext = "";
+            this.secondChart.DataContext = "";
+
+            string ticker = ((ComboBoxItem)this.TickerSelector.SelectedItem).Content.ToString().ToLower();
+
+            List<FinancialData> newData = new List<FinancialData>();
+            List<RootObject> stockData = await IEXDataProxy.GetData(duration, ticker);
+
+            foreach (var dataPoint in stockData)
+            {
+                DateTime nextDate = new DateTime();
+
+                if(DateTime.TryParse(dataPoint.date, out nextDate))
+                {
+                    newData.Add(new FinancialData()
+                    {
+                        High = dataPoint.high,
+                        Open = dataPoint.open,
+                        Low = dataPoint.low,
+                        Close = dataPoint.close,
+                        Date = nextDate
+                    });
+                }
+                else
+                {
+                    newData.Add(new FinancialData()
+                    {
+                        High = dataPoint.high,
+                        Open = dataPoint.open,
+                        Low = dataPoint.low,
+                        Close = dataPoint.close,
+                        Date = DateTime.Today
+                    });
+                }
+            }
+
+            this.OhlcChart.DataContext = newData;
+            this.secondChart.DataContext = newData;
+        }
+
+        private void Button_1_Day_Click(object sender, RoutedEventArgs e)
+        {   this.GenerateChart("1d");   }
+
+        private void Button_1_Month_Click(object sender, RoutedEventArgs e)
+        {   this.GenerateChart("1m");   }
+
+        private void Button_3_Month_Click(object sender, RoutedEventArgs e)
+        {   this.GenerateChart("3m");   }
+
+        private void Button_6_Month_Click(object sender, RoutedEventArgs e)
+        {   this.GenerateChart("6m");   }
+
+        private void Button_YTD_Click(object sender, RoutedEventArgs e)
+        {   this.GenerateChart("ytd");  }
+
+        private void Button_1_Year_Click(object sender, RoutedEventArgs e)
+        {   this.GenerateChart("1y");   }
+
+        private void Button_2_Year_Click(object sender, RoutedEventArgs e)
+        {   this.GenerateChart("2y");   }
+
+        private void Button_5_Year_Click(object sender, RoutedEventArgs e)
+        {   this.GenerateChart("5y");   }
+
+    }
+}
